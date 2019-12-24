a692d51616728a3cb68a0b1cc129f145d6ac8f76
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-12-24 17:18:55 +0000

    Remove TranslatedModelSerializer

--- geotrek/common/serializers.py
+++ geotrek/common/serializers.py
@@ -1,26 +1,11 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.db import models as django_db_models
 from django.shortcuts import get_object_or_404
 from django.utils.translation import get_language
 
 from rest_framework import serializers as rest_serializers
-from rest_framework import serializers as rest_fields
 
 from .models import Theme, RecordSource, TargetPortal, FileType, Attachment
-
-
-class TranslatedModelSerializer(rest_serializers.ModelSerializer):
-    def get_field(self, model_field):
-        kwargs = {}
-        if issubclass(model_field.__class__,
-                      (django_db_models.CharField,
-                       django_db_models.TextField)):
-            if model_field.null:
-                kwargs['allow_none'] = True
-            kwargs['max_length'] = getattr(model_field, 'max_length')
-            return rest_fields.CharField(**kwargs)
-        return super(TranslatedModelSerializer, self).get_field(model_field)
 
 
 class PictogramSerializerMixin(rest_serializers.ModelSerializer):
